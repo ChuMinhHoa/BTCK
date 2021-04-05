84d9de4e22a052b7c86be17c8efad17f21d65fd8
ChuHoa <74419130+ChuHoa@users.noreply.github.com>
2021-04-05 23:04:21 +0700

    upload

--- js/component/Upload-form.js
+++ js/component/Upload-form.js
@@ -6,7 +6,31 @@ $template.innerHTML =/*html */`
 integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
 integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-<link rel="stylesheet" href="./css/style.css"> 
+<link rel="stylesheet" href="./css/load-screen.css">
+<link rel="stylesheet" href="./css/style.css">
+<div id="load-screen" style="display:none;" class="fixed">
+<div  class="load-screen">
+    <div class='body'>
+        <span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+        <div class='base'>
+            <span></span>
+            <div class='face'></div>
+        </div>
+    </div>
+    <div class='longfazers'>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <h1>Redirecting</h1>
+</div>
+</div> 
 <div class="p-3">
 <div class="container">
     <div class="up-load-wrap">
@@ -16,10 +40,24 @@ integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwr
                     <label class="label">Input video name</label>
                     <input-wrapper id="video-name" type="text" placeholder="Video name..."></input-wrapper>
                 </div>
-                <div class="form-group">
-                    <label class="label">Input video link</label>
-                    <input-wrapper id="video-link" type="text" placeholder="Video link..."></input-wrapper>
+                <div class="form-group d-flex">
+                    <label class="label w-50">Choose video image</label>
+                    <div class="d-flex justify-content-end w-50">
+                        <input type="file" id="up-load-video" accept="video/*">
+                        <label for="up-load-video" class="label-for-img btn btn-primary fix form-control d-flex justify-content-center align-items-center"
+                        style="width:60%">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
+                                <path d="M.5 3l.04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.684.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
+                                <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
+                            </svg>
+                            Choose Video
+                        </label>
+                    </div>
                 </div>
+                <div id="video-choosen" style="height: 20px;
+                overflow: hidden;">
+                        
+                    </div>
                 <div class="form-group">
                     <label class="label">Choose type of video</label>
                     <select class="form-select form-select-lg mb-3" id="select-id">
@@ -31,7 +69,8 @@ integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwr
                         <label class="label w-50">Choose video image</label>
                         <div class="d-flex justify-content-end w-50">
                             <input type="file" id="up-load-img" accept="image/*">
-                            <label for="up-load-img" class="label-for-img btn btn-primary fix form-control d-flex justify-content-center align-items-center">
+                            <label for="up-load-img" class="label-for-img btn btn-primary fix form-control d-flex justify-content-center align-items-center"
+                            style="width:60%">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
                                     <path d="M.5 3l.04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.684.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
                                     <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
@@ -41,7 +80,12 @@ integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwr
                         </div>
                     </div>
                     <br>
-                    <div class="w-100 d-flex justify-content-center border border-dark rounded" style="max-height: 300px;height: 300px;">
+                    <div class="d-flex justify-content-center border border-dark rounded" style="
+                    max-height: 200px;
+                    height: 200px;
+                    margin:auto;
+                    max-width: 300px;
+                    width: 300px;">
                         <img class="w-100 rounded" src="" alt="" id="image">
                     </div>
                 </div>
@@ -72,35 +116,45 @@ export default class UploadForm extends HTMLElement {
 
         this.$select=this.shadowRoot.getElementById('select-id')
 
+        this.$loadScreen=this.shadowRoot.getElementById('load-screen')
+
+        this.$videochoosen=this.shadowRoot.getElementById('video-choosen')
+        this.$videoUpload = this.shadowRoot.getElementById('up-load-video')
+
     }
-    uploadImg() {
+    loadImg() {
         let reader = new FileReader();
         let file = this.$imgUpload.files[0];
         reader.addEventListener('load', (e) => {
             this.$img.src = e.target.result
+            console.log(file.name);
+        })
+        reader.readAsDataURL(file);
+    }
+    loadVideo(){
+        let reader=new FileReader();
+        let file = this.$videoUpload.files[0];
+        reader.addEventListener('load',()=>{
+            this.$videochoosen.innerHTML=file.name
         })
         reader.readAsDataURL(file);
     }
     async uploadVideo() {
         let ref = firebase.storage().ref();
 
-        let file = this.$imgUpload.files[0];
+        let fileImg = this.$imgUpload.files[0];
+        let fileVideo=this.$videoUpload.files[0];
 
-        let name = file.name;
+        let d=new Date();
+        let nameImg = d.getTime()+"_"+fileImg.name;
+        let nameVideo=d.getTime()+"_"+this.$videochoosen.innerHTML;
 
         let metaData = {
-            contentType: file.type
+            contentType: fileImg.type
         }
-
-        const task = ref.child(name).put(file, metaData)
-
-        let _linkImage;
-
-        await task.then(snapshot => snapshot.ref.getDownloadURL()).then(url => {
-            _linkImage = url
-        })
-
-        let _videoLink = changeLink(this.$videoLink.value);
+        let metaDataVideo={
+            contentType: fileVideo.type
+        }
 
         let _videoName = this.$videoName.value;
 
@@ -108,7 +162,23 @@ export default class UploadForm extends HTMLElement {
         let _idTypeVideo =this.$select.value;
         
         let isPssed=(this.$videoName.validate(checkNull,"Please input video name"))
+
         if (isPssed) {
+
+            let _linkImage;
+            let _videoLink;
+            
+            this.$loadScreen.style.display="block";
+            const task = ref.child(nameImg).put(fileImg, metaData)
+            await task.then(snapshot => snapshot.ref.getDownloadURL()).then(url => {
+                _linkImage = url
+            })
+
+            const taskVideo = ref.child(nameVideo).put(fileVideo,metaDataVideo)
+            await taskVideo.then(snapshot=>snapshot.ref.getDownloadURL()).then(url => {
+                _videoLink=url
+            })
+
             await firebase.firestore().collection('Video').add({
                 videoName: _videoName,
                 linkVideo: _videoLink,
@@ -116,7 +186,7 @@ export default class UploadForm extends HTMLElement {
                 typeID: _idTypeVideo,
                 userID: ''
             })
-            alert("Upload video successfully!");
+            this.$loadScreen.style.display="none";
             router.navigate('/home');
         }
     }
@@ -128,9 +198,7 @@ export default class UploadForm extends HTMLElement {
             $option.innerHTML=data.data().name
 
             this.$select.appendChild($option);
-            
         }
-
     }
     connectedCallback() {
         this.loadTypeVideo();
@@ -140,7 +208,11 @@ export default class UploadForm extends HTMLElement {
         }
 
         this.$imgUpload.onchange = () => {
-            this.uploadImg();
+            this.loadImg();
+        }
+        this.$videoUpload.onchange=()=>{
+            this.loadVideo();
+            console.log("dd");
         }
 
     }
--- js/component/video-form.js
+++ js/component/video-form.js
@@ -3,7 +3,7 @@ $template.innerHTML=/*html*/`
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="./css/style.css">   
 <div class="container video-div">
-    <video-wrappers videoname='hoa' id="video1" videosrc="https://www.youtube.com/embed/NPVvIE5ethU"></video-wrappers>
+    <video-wrappers videoname='hoa' id="video1" videosrc="https://firebasestorage.googleapis.com/v0/b/btck-564c6.appspot.com/o/1617629302283_NH%E1%BA%A0C%20TR%E1%BA%BA%20REMIX%202021%20HAY%20NH%E1%BA%A4T%20HI%E1%BB%86N%20NAY%20-%20EDM%20Tik%20Tok%20ORINN%20REMIX%20-%20Lk%20Nh%E1%BA%A1c%20Tr%E1%BA%BB%20Remix%20G%C3%A2y%20Nghi%E1%BB%87n%20Nh%E1%BA%A5t.mp4?alt=media&token=3dc9c4a1-0742-410c-8b42-a2128275ef3e"></video-wrappers>
 </div>`;
 export default class VideoForm extends HTMLElement {
     
