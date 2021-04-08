044689f3098d3797c89981e38440122222e6189f
PinkPig592002 <hoacmgch18105@gmail.com>
2021-04-08 19:32:42 +0700

    push

--- js/component/show-all-video.js
+++ js/component/show-all-video.js
@@ -2,8 +2,6 @@
 let $template = document.createElement("template");
 $template.innerHTML=/*html */
 `
-<link rel="stylesheet" href="./css/home.css">
-<link rel="stylesheet" href="./css/style.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -11,6 +9,9 @@ $template.innerHTML=/*html */
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <!-- The core Firebase JS SDK is always required and must be listed first -->
+    
+<link rel="stylesheet" href="./css/home.css">
+<link rel="stylesheet" href="./css/style.css">
 <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn " id="close">&times;</a>
         <div id="show-pf">
